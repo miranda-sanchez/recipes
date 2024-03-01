66078f89122ceb5d36f6b6c1f05fd953b5092dc3
miranda-sanchez <121776162+miranda-sanchez@users.noreply.github.com>
2024-03-01 13:04:18 -0300

    Changes in About.js 03-01-2024

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import img1 from "../img/plate1.png";
+import img2 from "../img/plate2.png";
 
 const About = () => {
   return (
-    <main>
-      <h2>About Us</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-        molestiae sed quidem porro unde quia veritatis pariatur temporibus illum
-        incidunt? Quidem odio blanditiis veritatis dolorum aliquam porro dolor
-        doloremque unde.
-      </p>
-      <Link to="/">
-        <button>Go back Home</button>
-      </Link>
+    <main className="AboutUs">
+      <section className="content">
+        <h2>About Us</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          molestiae sed quidem porro unde quia veritatis pariatur temporibus
+          illum incidunt? Quidem odio blanditiis veritatis dolorum aliquam porro
+          dolor doloremque unde.
+        </p>
+        <Link to="/">
+          <button className="back-home-btn">Go back Home</button>
+        </Link>
+      </section>
+      <figure>
+        <img src={img1} alt="Illustration of a plate with food" />
+        <img src={img2} alt="Illustration of a plate with pizza" />
+      </figure>
     </main>
   );
 };
