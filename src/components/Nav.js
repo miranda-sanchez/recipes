858093266eb38401998a1de5b246497475c6c316
miranda-sanchez <121776162+miranda-sanchez@users.noreply.github.com>
2024-03-25 19:38:30 -0300

    Changes in the visibility of the navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaLinkedin, FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import logo from "../img/logo.png";
 
 const Nav = () => {
+  // Controlling the visibility of the modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Closing the modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
@@ -15,37 +23,42 @@ const Nav = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-link" to="/" onClick={closeModal}>
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">
+              <Link className="nav-link" to="/about" onClick={closeModal}>
                 About
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/lets-talk">
+              <Link to="/lets-talk" onClick={closeModal}>
                 <button>Let's talk</button>
               </Link>
             </li>
           </ul>
         </div>
 
-        {/* Button trigger modal */}
+        {/* Navbar for small screens */}
+        {/* Button to trigger the modal */}
         <div className="mobile-toggler d-lg-none">
-          <button data-bs-toggle="modal" data-bs-target="#navModal">
+          <button
+            data-bs-toggle="modal"
+            data-bs-target="#navModal"
+            onClick={() => setIsModalOpen(true)}
+          >
             <FaBars />
           </button>
         </div>
 
         {/* Modal */}
         <div
-          className="modal fade"
+          className={`modal fade ${isModalOpen ? "show" : ""}`}
           id="navModal"
           tabIndex="-1"
           aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
+          aria-hidden={!isModalOpen}
           data-bs-backdrop="false"
         >
           <div className="modal-dialog">
@@ -57,23 +70,32 @@ const Nav = () => {
                   className="btn-close"
                   data-bs-dismiss="modal"
                   aria-label="Close"
+                  onClick={closeModal}
                 ></button>
               </div>
 
               <div className="modal-body">
                 <ul>
                   <li className="modal-line">
-                    <Link className="nav-link" to="/">
+                    <Link
+                      className="nav-link"
+                      to="/"
+                      onClick={() => setIsModalOpen(false)}
+                    >
                       Home
                     </Link>
                   </li>
                   <li className="modal-line">
-                    <Link className="nav-link" to="/about">
+                    <Link
+                      className="nav-link"
+                      to="/about"
+                      onClick={() => setIsModalOpen(false)}
+                    >
                       About
                     </Link>
                   </li>
                   <li>
-                    <Link to="/lets-talk">
+                    <Link to="/lets-talk" onClick={() => setIsModalOpen(false)}>
                       <button>Let's talk</button>
                     </Link>
                   </li>
@@ -83,12 +105,18 @@ const Nav = () => {
               <div className="modal-footer">
                 <ul>
                   <li>
-                    <Link to="https://www.linkedin.com/in/miranda--sanchez/">
+                    <Link
+                      to="https://www.linkedin.com/in/miranda--sanchez/"
+                      onClick={closeModal}
+                    >
                       <FaLinkedin className="icon" />
                     </Link>
                   </li>
                   <li>
-                    <Link to="https://github.com/miranda-sanchez">
+                    <Link
+                      to="https://github.com/miranda-sanchez"
+                      onClick={closeModal}
+                    >
                       <FaGithub className="icon" />
                     </Link>
                   </li>
